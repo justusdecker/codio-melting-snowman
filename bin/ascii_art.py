ebe01fdb24c39763333cb97b1f5b74998a1e369c
Justus <justus.d2025@gmail.com>
2025-05-28 10:24:42 +0200

    added ascii & emoji art

---
+++ bin/ascii_art.py
@@ -0,0 +1,49 @@
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
+#I will implement a emoji version
+
+EMOJI_STAGES = [
+"""
+ 🎃
+ ⚪
+ ⚪
+""",
+"""
+ 🎃
+ ⚪
+""",
+"""
+ 🎃
+""",
+"""
+ 💧
+"""
+]
